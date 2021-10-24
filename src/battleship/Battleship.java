00fbd9fda39089f4e396f694782976971d62f71b
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-10-24 20:29:55 +0300

    add ships on the battlefield

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -3,21 +3,24 @@ package battleship;
 import java.util.Scanner;
 
 public class Battleship {
+    static String player1 = "player1";
+    static String player2 = "player2";
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String exit, player1, player2;
-        int[][] arr = new int[10][10];
-        System.out.println("Игрок 1 введите ваше имя:");
-        player1 = sc.next();
-        System.out.println("Игрок 2 введите ваше имя:");
-        player2 = sc.next();
+        String exit;
+        int[][] arr1 = new int[10][10];
+        int[][] arr2 = new int[10][10];
+        //System.out.println("Игрок 1 введите ваше имя:");
+        //player1 = sc.next();
+        //System.out.println("Игрок 2 введите ваше имя:");
+        //player2 = sc.next();
         do {
             System.out.println("Для выхода из игры введите 'exit'");
             field();
-            arr = fieldFilling(player1);
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    System.out.print(arr[i][j]);
+            arr1 = fieldFilling(player1);
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr1.length; j++) {
+                    System.out.print(arr1[i][j]);
                 }
                 System.out.println();
             }
@@ -38,20 +41,36 @@ public class Battleship {
         int[][] arr = new int[10][10];
         int shipCoord1, shipCoord2;
         int position;
+        int deck = 4;
         System.out.println(name + " вводит координаты кораблей");
-        System.out.println("Четырёхпалубный корабль:");
-        shipCoord1 = sc.nextInt();
-        shipCoord2 = sc.nextInt();
-        arr[shipCoord1][shipCoord2] = 1;
-        System.out.println("1. Расположить вертикально.");
-        System.out.println("2. Раположить горизонтально.");
-        position = sc.nextInt();
-        if (position == 1) {
-            for (int i = shipCoord1; i < shipCoord1 + 4 ; i++) {
-                arr[i][shipCoord2] = 1;
+        do {
+            System.out.println(deck + "-палубный корабль:");
+            System.out.println("Координата х:");
+            shipCoord1 = sc.nextInt();
+            System.out.println("Координата у:");
+            shipCoord2 = sc.nextInt();
+            arr[shipCoord1][shipCoord2] = 1;
+            System.out.println("1. Расположить вертикально.");
+            System.out.println("2. Раположить горизонтально.");
+            position = sc.nextInt();
+            for (int i = 0; i < deck; i++) {
+                if (position == 1) {
+                    arr[shipCoord1 + i][shipCoord2] = 1;
+                } else {
+                    arr[shipCoord1][shipCoord2 + i] = 1;
+                }
             }
-        }
+            deck--;
+        }while(deck !=0);
         return arr;
     }
+    static void surroundShip(int[][] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] != 1 && i + 1 < arr.length) {
 
+                }
+            }
+        }
+    }
 }
